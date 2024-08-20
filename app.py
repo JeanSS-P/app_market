0f5771fd18846bfa376405e031e456c2037aec66
Jean <jeansilvadsantos@gmail.com>
2024-08-20 19:59:52 -0300

    atualização do requirements e app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask,render_template
-
-
+from dotenv import load_dotenv
+import os
 app = Flask(__name__)
 
-
+app.secret_key = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
-
+@app.route('/produtos')
+def index():
+    return render_template('produtos.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
